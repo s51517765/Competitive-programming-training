ecd7b41ebf07d34ddd4aeece30845930cf8837cc
s51517765 <30945690+s51517765@users.noreply.github.com>
2020-01-06 22:50:53 +0900

    https://atcoder.jp/contests/abc043/tasks/abc043_b

---
+++ 3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// https://atcoder.jp/contests/abc043/tasks/abc043_b
+
+int main()
+{
+    string s;
+    cin >> s;
+    string t;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (s[i] == 'B')
+        {
+            t.resize(t.size() - 1); //サイズを一つ小さくすることで最後の1文字を削る
+        }
+        else
+        {
+            t.push_back(s[i]);
+        }
+    }
+    cout << t << endl;
+    return 0;
+}
